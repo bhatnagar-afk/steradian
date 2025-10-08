57d1d3dd78a80851a5392c304eb533c1b0caf680
bhatnagar-afk <bhatnagarkush099@gmail.com>
2025-10-09 00:05:34 +0530

    fix: update environment variable names for emailjs integration

--- src/lib/services/email-sender.tsx
+++ src/lib/services/email-sender.tsx
@@ -1,14 +1,13 @@
 import emailjs from "emailjs-com";
-
 export const sendEmail = (formData: { name: string; email: string; message: string }) => {
   return emailjs.send(
-    process.env.emailJS_SERVICE_ID || "",
-    process.env.emailJS_TEMPLATE_ID || "",
+    process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
+    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
     {
       name: formData.name,
       email: formData.email,
       message: formData.message,
     },
-    process.env.emailJS_PUBLIC_KEY || ""
+    process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || ""
   );
 };
