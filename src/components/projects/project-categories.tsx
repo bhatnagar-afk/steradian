700ec93d190091c8cacd59cf654fb998ae78af79
bhatnagar-afk <bhatnagarkush099@gmail.com>
2026-01-03 16:44:50 +0530

    feat: enhance project descriptions and improve category handling
    - Updated ProjectsPage to display active category description dynamically.
    - Refactored Description component for improved text display logic.
    - Simplified ProjectCategories component by removing unused theme configuration.
    - Modified getCategories function to include category descriptions.

--- src/components/projects/project-categories.tsx
+++ src/components/projects/project-categories.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter, useSearchParams } from 'next/navigation'
-import { fonts, colors, themes } from '@/config/theme'
+import { fonts, colors } from '@/config/theme'
 import { Button } from '../ui/button'
 
 type ProjectCategoriesProps = {
@@ -17,7 +17,6 @@ export default function ProjectCategories({
 }: ProjectCategoriesProps) {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const themeConfig = themes.dark
 
   const handleClick = (id: string) => {
     const params = new URLSearchParams(searchParams.toString())
@@ -39,23 +38,20 @@ export default function ProjectCategories({
             style={{
               fontFamily: fonts.sans,
               color: isActive
-                ? themes.light.background
-                : 'rgba(255, 255, 255, 0.75)',
-              // backgroundColor: isActive ? colors.primary : 'transparent',
-              transform: isActive ? 'scale(1.1)' : 'scale(1)',
+                ? colors.white
+                : colors.gray[400],
+              transform: isActive ? 'scale(1.25)' : 'scale(1)',
             }}
             onMouseEnter={(e) => {
               if (!isActive) {
-                // e.currentTarget.style.backgroundColor = themeConfig.hover
                 e.currentTarget.style.transform = 'scale(1.1)'
-                e.currentTarget.style.color = themes.light.background
+                e.currentTarget.style.color = colors.white
               }
             }}
             onMouseLeave={(e) => {
               if (!isActive) {
-                // e.currentTarget.style.backgroundColor = 'transparent'
                 e.currentTarget.style.transform = 'scale(1)'
-                e.currentTarget.style.color = 'rgba(255, 255, 255, 0.75)'
+                e.currentTarget.style.color = colors.gray[400]
               }
             }}
           >
