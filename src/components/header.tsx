01c7aad51c5669c0e006c3fe5cc0f58ec32af811
bhatnagar-afk <bhatnagarkush099@gmail.com>
2026-01-04 23:46:17 +0530

    fix: remove unnecessary margin from logo in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
               width={32}
               height={32}
               alt="Steradian Logo"
-              className="rounded-full mr-10"
+              className="rounded-full"
             />
           </span>
           <span className="text-xl font-semibold">Steradian Architects</span>
